9768d67d331904433bed29782a32880f5c7c6a2e
JERU156 <PAVITHRATAIGA@GMAIL.COM>
2021-10-08 09:40:51 +0530

    TESTING CALCULTOR
    add,sub,mul or div

---
+++ Scripts/Script3.js
@@ -0,0 +1,28 @@
+function add()
+{
+  input1=document.getElementById('input1');
+  input2=document.getElementById('input2');
+  sum=parseInt(input1.value)+parseInt(input2.value);
+  alert(sum);
+}
+function sub()
+{
+  input1=document.getElementById('input1');
+  input2=document.getElementById('input2');
+  subtraction=parseInt(input1.value)-parseInt(input2.value);
+  alert(subtraction);
+}
+function mul()
+{
+  input1=document.getElementById('input1');
+  input2=document.getElementById('input2');
+  multiply=parseInt(input1.value)*parseInt(input2.value);
+  alert(multiply)
+}
+function division()
+{
+  input1=document.getElementById('input1');
+  input2=document.getElementById('input2');
+  output_div=parseInt(input1.value)/parseInt(input2.value);
+  alert(output_div)
+}
